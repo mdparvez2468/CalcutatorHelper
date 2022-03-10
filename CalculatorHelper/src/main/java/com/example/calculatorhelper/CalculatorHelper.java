8144b340c5c12a48af2fd5dd17db4bcd3c3baf9b
Md Parvez <parvez15-2468@diu.edu.bd>
2022-03-11 01:16:58 +0600

    First Commit

---
+++ CalculatorHelper/src/main/java/com/example/calculatorhelper/CalculatorHelper.java
@@ -0,0 +1,22 @@
+package com.example.calculatorhelper;
+
+public class CalculatorHelper {
+
+    public static int sum(int x, int y){
+        return x+y;
+    }
+
+    public static int sub(int x, int y){
+        return x+y;
+    }
+
+    public static int mul(int x, int y){
+        return x+y;
+    }
+
+    public static int div(int x, int y){
+        return x+y;
+    }
+
+
+}
